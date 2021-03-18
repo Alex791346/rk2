4292b8c7c9c41cbd983d950bd507ea3991cdadac
Alexander Chen Lee <63467490+Alex791346@users.noreply.github.com>
2021-03-18 15:49:54 +0600

    Изменения

---
+++ Desktop/rk1/src/base.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+ apiKey: "AIzaSyCBA-knBZDQflqaGYZwTVtiy-HnvqcDcGI",
+  authDomain: "rk2back.firebaseapp.com",
+  projectId: "rk2back",
+  storageBucket: "rk2back.appspot.com",
+  messagingSenderId: "218691211568",
+  appId: "1:218691211568:web:26660909ddd109e46c78f5",
+  measurementId: "G-JZR2N7DK1B"
+});
+
+export default app;
